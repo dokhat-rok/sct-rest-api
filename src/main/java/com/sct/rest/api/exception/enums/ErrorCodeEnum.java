b11e5b0dca176478e14430a3f871990dcac5f786
dmitry.krivenko <dima0409.crivenko@yandex.ru>
2023-03-08 23:31:29 +0300

    Перенес ServiceRuntimeException и ErrorCodeEnum в отдельный пакет

--- src/main/java/com/sct/rest/api/exception/enums/ErrorCodeEnum.java
+++ src/main/java/com/sct/rest/api/exception/enums/ErrorCodeEnum.java
@@ -1,4 +1,4 @@
-package com.sct.rest.api.service;
+package com.sct.rest.api.exception.enums;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -6,28 +6,38 @@ import org.springframework.http.HttpStatus;
 
 import java.text.MessageFormat;
 
-@RequiredArgsConstructor
 @Getter
+@RequiredArgsConstructor
 public enum ErrorCodeEnum {
+
     INTERNAL_SERVER_ERROR("Внутренняя ошибка сервиса"),
+
     USER_NOT_FOUND("Пользователь не найден, id={0}", HttpStatus.NOT_FOUND),
+
     PARKING_NOT_FOUND("Парковка не найдена, id={0}", HttpStatus.NOT_FOUND),
+
     TRANSPORT_NOT_FOUND("Транспорт не найден, id={0}", HttpStatus.NOT_FOUND),
+
     RENT_NOT_FOUND("Аренда не найдена, id={0}", HttpStatus.NOT_FOUND),
+
     TRANSPORT_NOT_AVAILABLE("Транспорт недоступен"),
+
     NO_MONEY("Недостаточно денежных средств"),
+
     VALIDATION_ERROR("Ошибка валидации"),
+
     ERROR_AUTHORIZATION("Ошибка авторизации", HttpStatus.UNAUTHORIZED);
+
     private HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+
     private final String messageTemplate;
 
-
-    ErrorCodeEnum(String messageTemplate, HttpStatus httpStatus){
+    ErrorCodeEnum(String messageTemplate, HttpStatus httpStatus) {
         this.messageTemplate = messageTemplate;
         this.httpStatus = httpStatus;
     }
 
-    public String getMessage(Object... args){
+    public String getMessage(Object... args) {
         return MessageFormat.format(messageTemplate, args);
     }
 }
