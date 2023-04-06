2e617b683f1c234cada8e5dfaaf6b7c174f3a132
dmitry.krivenko <dima0409.crivenko@yandex.ru>
2023-04-06 04:30:41 +0300

    Добавил метод пагинации и фильтрации в репозиториях сущностей

--- src/main/java/com/sct/rest/api/repository/CustomerRepository.java
+++ src/main/java/com/sct/rest/api/repository/CustomerRepository.java
@@ -1,11 +1,19 @@
 package com.sct.rest.api.repository;
 
 import com.sct.rest.api.model.entity.CustomerEntity;
+import com.sct.rest.api.model.enums.Role;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
 
     Optional<CustomerEntity> findByLogin(String login);
+
+    @Query("select customer from CustomerEntity customer where (:login is null or customer.login like %:login%) " +
+            "and (:role is null or customer.role = :role)")
+    Page<CustomerEntity> findAllByFilter(Pageable pageable, String login, Role role);
 }
