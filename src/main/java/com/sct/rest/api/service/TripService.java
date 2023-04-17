7d5ef814c98808d968128a3f3233d44ba843642d
dmitry.krivenko <krivenko_da@nlmk.com>
2023-04-18 01:51:06 +0300

    Добавил транзакции для начала и окончания поездки

--- src/main/java/com/sct/rest/api/service/TripService.java
+++ src/main/java/com/sct/rest/api/service/TripService.java
@@ -3,6 +3,7 @@ package com.sct.rest.api.service;
 import com.sct.rest.api.model.dto.RentDto;
 import com.sct.rest.api.model.dto.trip.TripBeginDto;
 import com.sct.rest.api.model.dto.trip.TripEndDto;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Сервис с бизнес-логикой связанной с арендами пользователей
@@ -15,6 +16,7 @@ public interface TripService {
      * @param tripBegin Параметры для начала поездки {@link TripBeginDto}
      * @return Созданная поездка {@link RentDto}
      */
+    @Transactional
     RentDto beginRent(TripBeginDto tripBegin);
 
     /**
@@ -23,5 +25,6 @@ public interface TripService {
      * @param tripEnd Параметры для окончания поездки {@link TripEndDto}
      * @return Оконченная поездка {@link RentDto}
      */
+    @Transactional
     RentDto endRent(TripEndDto tripEnd);
 }
