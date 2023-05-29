f84e815d1a8f9b9086c770877903a9237925e82a
dmitry.krivenko <krivenko_da@nlmk.com>
2023-05-29 04:30:55 +0300

    Сделал сортировку геопозиций маршрута по времени

--- src/main/java/com/sct/rest/api/service/impl/RentServiceImpl.java
+++ src/main/java/com/sct/rest/api/service/impl/RentServiceImpl.java
@@ -3,6 +3,7 @@ package com.sct.rest.api.service.impl;
 import com.sct.rest.api.mapper.rent.RentMapper;
 import com.sct.rest.api.model.dto.RentDto;
 import com.sct.rest.api.model.entity.RentEntity;
+import com.sct.rest.api.model.entity.RoutePointEntity;
 import com.sct.rest.api.model.enums.RentStatus;
 import com.sct.rest.api.model.filter.RentPageableFilter;
 import com.sct.rest.api.repository.RentRepository;
@@ -14,7 +15,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -33,6 +36,9 @@ public class RentServiceImpl implements RentService {
     public List<RentDto> getAllRentForCurrentUserByStatus(RentStatus status) {
         List<RentEntity> rentList = rentRepository
                 .findAllByCustomerLoginAndStatus(SecurityContext.get().getCustomerLogin(), status);
+        rentList.forEach(r -> r.setRoutePoints(r.getRoutePoints().stream()
+                .sorted(Comparator.comparing(RoutePointEntity::getCreated_date))
+                .collect(Collectors.toList())));
         return rentMapper
                 .toListDto(rentList);
     }
